4bc35aca0f85ee0e2dae7a2a92dc8b10453894bc
azqa.amanullah1231@gmail.com <azqa.amanullah1231@gmail.com>
2026-08-14 14:39:50 +0100

    Personalize portfolio home page

--- Frontend/obj/Release/net9.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Release/net9.0/Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e08d2111085f550da9d5c5a9df2d821770a41d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4bfa9fb10152c15163a6de2614829867823f102")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
