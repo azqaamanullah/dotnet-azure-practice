561238e0d9d063104184d198601ce7accaa05e96
azqa.amanullah1231@gmail.com <azqa.amanullah1231@gmail.com>
2026-08-16 01:21:46 +0100

    new pages added

--- Frontend/Models/Project.cs
+++ Frontend/Models/Project.cs
@@ -1,16 +1,12 @@
-namespace dotnet_azure_practice.Models;
+namespace Frontend.Models;
 
 public class Project
 {
-    public string Title { get; set; } = string.Empty;
+    public string Title { get; set; } = "";
 
-    public string Description { get; set; } = string.Empty;
+    public string Description { get; set; } = "";
 
-    public string[] Technologies { get; set; } = [];
+    public List<string> Technologies { get; set; } = [];
 
-    public string GitHubUrl { get; set; } = "#";
-
-    public string LiveUrl { get; set; } = "#";
-
-    public string Image { get; set; } = string.Empty;
+    public string GitHubUrl { get; set; } = "";
 }
