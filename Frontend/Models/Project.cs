c3a3248e68f377e298fe950ae00daf4effd72efb
azqa.amanullah1231@gmail.com <azqa.amanullah1231@gmail.com>
2026-08-16 01:36:29 +0100

    Error removed

--- Frontend/Models/Project.cs
+++ Frontend/Models/Project.cs
@@ -5,6 +5,8 @@ public class Project
     public string Title { get; set; } = "";
 
     public string Description { get; set; } = "";
+    public string Image { get; set; } = string.Empty;
+    public string LiveUrl { get; set; } = string.Empty;
 
     public List<string> Technologies { get; set; } = [];
 
